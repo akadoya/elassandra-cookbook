462967d1ec74e5e1824498767940ce4d8f3c0e57
Kadoya, Aoi | ECCO <aoi.kadoya@rakuten.com>
2017-11-02 17:24:13 -0700

    updating the default version

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,11 +6,11 @@
 #
 
 default['elassandra']['install_type'] = 'tar'
-default['elassandra']['release_version'] = '2.4.5.3'
+default['elassandra']['release_version'] = '5.5.0.3'
 default['elassandra']['install_prefix'] = '/opt/elassandra'
 
 default['elassandra']['tar']['download_url'] = "https://github.com/strapdata/elassandra/releases/download/v#{node['elassandra']['release_version']}/elassandra-#{node['elassandra']['release_version']}.tar.gz"
-default['elassandra']['tar']['checksum'] = '34b8599a9806870b3fbe7e638fb38233dd1df459b9601efa724b9b2c4023a4b3'
+default['elassandra']['tar']['checksum'] = '6961c9feca886422b396b6c10735363daea89f632c462f1983740fa9998dc679'
 
 default['elassandra']['pid'] = '/var/run/cassandra/cassandra.pid'
 default['elassandra']['user'] = 'cassandra'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cadyan.aoi@gmail.com'
 license 'Apache 2.0'
 description 'Installs elassandra'
 long_description 'Installs elassandra'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.9' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/akadoya/elassandra-cookbook/issues'
